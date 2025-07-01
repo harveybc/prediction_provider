1d407b361584fd4a8295ed60fd2dea8983e49b32
harveybc <harveybc@ingeni-us.com>
2025-06-30 22:16:47 -0500

    update

--- setup.py
+++ setup.py
@@ -10,33 +10,29 @@ setup(
         ],
         # Pipeline plugins - Orquestan el flujo de procesamiento completo
         'pipeline.plugins': [
-            'default_pipeline=pipeline_plugins.default_pipeline:Plugin'
+            'default_pipeline=plugins_pipeline.default_pipeline:Plugin'
         ],
         # Data feeder plugins - Obtienen y preparan datos de entrada
-        'data_feeder.plugins': [
-            'default_data_feeder=data_feeder_plugins.default_data_feeder:Plugin',
-            'file_data_feeder=data_feeder_plugins.file_data_feeder:Plugin',
-            'api_data_feeder=data_feeder_plugins.api_data_feeder:Plugin'
+        'feeder.plugins': [
+            'default_feeder=plugins_feeder.api_feeder:Plugin',
+            'file_feeder=plugins_feeder.file_feeder:Plugin',
+            'api_feeder=plugins_feeder.api_feeder:Plugin'
         ],
         # Predictor plugins - Cargan modelos y generan predicciones
         'predictor.plugins': [
-            'default_predictor=predictor_plugins.default_predictor:Plugin',
-            'keras_predictor=predictor_plugins.keras_predictor:Plugin',
-            'onnx_predictor=predictor_plugins.onnx_predictor:Plugin',
-            'remote_predictor=predictor_plugins.remote_predictor:Plugin'
+            'default_predictor=plugins_predictor.default_predictor:Plugin'            
         ],
         # API endpoints plugins - Definen endpoints RESTful individuales
-        'api_endpoints.plugins': [
-            'predict_endpoint=api_endpoints_plugins.predict_endpoint:Plugin',
-            'health_endpoint=api_endpoints_plugins.health_endpoint:Plugin',
-            'info_endpoint=api_endpoints_plugins.info_endpoint:Plugin',
-            'metrics_endpoint=api_endpoints_plugins.metrics_endpoint:Plugin'
+        'endpoints.plugins': [
+            'default_endpoints=plugins_endpoints.predict_endpoint:Plugin',
+            'predict_endpoint=plugins_endpoints.predict_endpoint:Plugin',
+            'health_endpoint=plugins_endpoints.health_endpoint:Plugin',
+            'info_endpoint=plugins_endpoints.info_endpoint:Plugin',
+            'metrics_endpoint=plugins_endpoints.metrics_endpoint:Plugin'
         ],
         # API core plugins - Gestionan configuración central del servidor Flask
-        'api_core.plugins': [
-            'default_api_core=api_core_plugins.default_api_core:Plugin',
-            'jwt_auth_core=api_core_plugins.jwt_auth_core:Plugin',
-            'cors_enabled_core=api_core_plugins.cors_enabled_core:Plugin'
+        'core.plugins': [
+            'default_core=plugins_core.default_core:Plugin'
         ]
     },
     install_requires=[
